3c15b81c6c7ed339e398794ac279eebb8ea4917e
EvgenyMinin <evgeny.frontend@gmail.com>
2021-11-11 21:41:32 +0300

    added events for guests

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,12 +1,26 @@
 import { Calendar } from 'antd';
+import { Moment } from 'moment';
 import React from 'react';
+import { useAppSelector } from '../hooks/redux';
 import { Event } from '../models';
+import { formatDate } from '../utils/formatDate';
 
 interface EventCalendarProps {
     events: Event[] | undefined;
 }
 
 export const EventCalendar = ({ events }: EventCalendarProps) => {
-    console.log('events cal', events);
-    return <Calendar />;
+    const { username } = useAppSelector((state) => state.auth.user);
+    const dateCellRender = (value: Moment) => {
+        const currentDayEvents = events?.filter((event) => event.date === formatDate(value) && event.guest === username);
+        return (
+            <div>
+                {currentDayEvents?.map((ev) => (
+                    <div key={ev.id}>{ev.description}</div>
+                ))}
+            </div>
+        );
+    };
+
+    return <Calendar dateCellRender={dateCellRender} />;
 };
